9b9bfe1097c4a4ab756c18c4ac45a801f30d1586
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2022-04-25 01:10:14 -0400

    Created the documents for the database.

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([.-]?)*\w+@\w+.\w{2,4}$/]
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }]
+    },
+    {
+    toJSON: {
+        virtuals: true,
+    },
+    id: false
+    }
+)
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+UserSchema.virtual('thoughtCount').get(function() {
+    return this.thoughts.length;
+});
+
+const User = model('User', UserSchema);
+
+//Export Users module
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,6 @@
+//Require Models
+const Thought = require('./Thought');
+const User = require('./User');
+
+//Export Modules
+module.exports = { User, Thought };
